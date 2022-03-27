e2396db4c51ee4d6180846087ef97066a3b20248
Juan Ramirez Jardua <lch_jramirez@hotmail.com>
2022-03-27 15:44:18 -0300

    first commit

--- ping2rgbv2.py
+++ ping2rgbv2.py
@@ -7,7 +7,7 @@ from random import randint
 #import date
 import datetime
 
-pixels = neopixel.NeoPixel(board.D18,8)
+pixels = neopixel.NeoPixel(board.D21,8)
 
 
 def enciendeRGB(led, estado, luminosidad):
@@ -24,31 +24,44 @@ def randomRGB(led, luminosidad):
    pixels[led] = (r,g,b)
    return
 
-p0 = 0 == os.system('ping -qc 1 www.facebook.com     >> /dev/null')
-p1 = 0 == os.system('ping -qc 1 www.bci.cl     >> /dev/null')
-p2 = 0 == os.system('ping -qc 1 www.tevapharm.com     >> /dev/null')
-p3 = 0 == os.system('ping -qc 1 www.instagram.com     >> /dev/null')
-p4 = 0 == os.system('ping -qc 1 www.latercera.com     >> /dev/null')
-p5 = 0 == os.system('ping -qc 1 www.amazon.com     >> /dev/null')
-p6 = 0 == os.system('ping -qc 1 www.pcfactory.cL   >> /dev/null')
-
 now = datetime.datetime.now()
 if now >= now.replace(hour=8) and now <= now.replace(hour=22):
    lumi = int(sys.argv[1] )
 else:
    lumi = int(sys.argv[2] )
- 
+
+for led in range(0,8):
+    randomRGB(led,lumi)
+
+p0 = 0 == os.system('ping -qc 3 www.facebook.com     >> /dev/null')
 enciendeRGB(0, p0, lumi)
+
+p1 = 0 == os.system('ping -qc 3 www.bci.cl     >> /dev/null')
 enciendeRGB(1, p1, lumi)
+
+p2 = 0 == os.system('ping -qc 3 www.tevapharm.com     >> /dev/null')
 enciendeRGB(2, p2, lumi)
+
+p3 = 0 == os.system('ping -qc 3 www.instagram.com     >> /dev/null')
 enciendeRGB(3, p3, lumi)
+
+p4 = 0 == os.system('ping -qc 3 www.latercera.com     >> /dev/null')
 enciendeRGB(4, p4, lumi)
+
+p5 = 0 == os.system('ping -qc 3 www.amazon.com     >> /dev/null')
 enciendeRGB(5, p5, lumi)
+
+p6 = 0 == os.system('ping -qc 3 www.pcfactory.cL   >> /dev/null')
 enciendeRGB(6, p6, lumi)
-randomRGB(7, lumi)
+
+p7 = 0 == os.system('ping -qc 3 www.microsoft.com   >> /dev/null')
+enciendeRGB(7, p7, lumi)
+
+
+
 
 dt_string = now.strftime("%d-%m-%Y %H:%M:%S")
-if (not p0 and not p1 and not p2 and not p3 and not p4 and not p5 and not p6):
+if (not p0 and not p1 and not p2 and not p3 and not p4 and not p5 and not p6 and not p7):
     f = open("/home/pi/ErrorRed.txt","a")
     f.write('Error de conexión internet a las %s\n\r'% ( dt_string))
     f.close()
--- rgb-set.py
+++ rgb-set.py
@@ -1,7 +1,7 @@
 import sys
 import board
 import neopixel
-pixels = neopixel.NeoPixel(board.D18,8)
+pixels = neopixel.NeoPixel(board.D10,8)
 luminosidad = int( sys.argv[3] )
 if sys.argv[2] == '1':
    pixels[int(sys.argv[1])] = (0,luminosidad,0)
